13dae4e6dafe2b56de93ad3789c71e1061b706aa
ncaq <ncaq@ncaq.net>
2014-03-20 03:42:37 +0900

    とりあえずの設定

---
+++ xmonad.hs
@@ -0,0 +1,23 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+
+main :: IO ()
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+
+myBar :: String
+myBar = "xmobar"
+
+myPP :: PP
+myPP = defaultPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+
+toggleStrutsKey :: XConfig t -> (KeyMask, KeySym)
+toggleStrutsKey _ = (mod4Mask, xK_F11)
+
+myConfig :: XConfig (Choose Tall (Choose (Mirror Tall) Full))
+myConfig = defaultConfig
+  {
+    terminal = "lilyterm",
+    modMask = mod4Mask,
+    borderWidth = 0,
+    focusFollowsMouse = False
+  }
