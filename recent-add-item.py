6fe8e51e82b7d88a47a17bda414976d5048b9517
ncaq <ncaq@ncaq.net>
2018-05-03 17:02:30 +0900

    cleaned: pep8

--- recent-add-item.py
+++ recent-add-item.py
@@ -7,6 +7,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GObject
 
-Gtk.RecentManager.get_default().add_item('file://' + os.path.abspath(sys.argv[1]))
+Gtk.RecentManager.get_default().add_item(
+    'file://' + os.path.abspath(sys.argv[1]))
 GObject.idle_add(Gtk.main_quit)
 Gtk.main()
