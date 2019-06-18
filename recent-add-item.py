66bc9231522ef2217f8c6d9cc5ba10ebdae54f37
ncaq <ncaq@ncaq.net>
2019-06-18 17:41:16 +0900

    fixed: recent-add-item.py: Gtkをロードする前にバージョンを設定する必要がある
    importで副作用起きるのが普通なのでlspのformatに従えなくてつらい.

--- recent-add-item.py
+++ recent-add-item.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
+import gi
+gi.require_version('Gtk', '3.0')
+
+# Gtkをロードする前にバージョンを設定する必要がある
+
 from gi.repository import Gtk
 from gi.repository import GObject
 import os.path
 import sys
-import gi
-
-gi.require_version('Gtk', '3.0')
 
 Gtk.RecentManager.get_default().add_item(
     'file://' + os.path.abspath(sys.argv[1]))
