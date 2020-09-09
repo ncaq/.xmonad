b0178d28ce84d2fdd2fdaec9d0a2113ca240804f
ncaq <ncaq@ncaq.net>
2020-09-09 21:16:19 +0900

    cleaned: recent-add-item: indent

--- recent-add-item.py
+++ recent-add-item.py
@@ -7,7 +7,7 @@ import gi
 gi.require_version('Gtk', '3.0')  # Gtkをロードする前にバージョンを設定する必要がある
 from gi.repository import Gtk, GObject  # noqa: E402
 
-Gtk.RecentManager.get_default().add_item(
-    'file://' + os.path.abspath(sys.argv[1]))
+Gtk.RecentManager.get_default().add_item('file://' +
+                                         os.path.abspath(sys.argv[1]))
 GObject.idle_add(Gtk.main_quit)
 Gtk.main()
