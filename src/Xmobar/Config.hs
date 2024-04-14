36927168e23cebebda1c8ef5e2444b5ee81a4226
ncaq <ncaq@ncaq.net>
2024-04-14 19:36:19 +0900

    refactor: xmobarの更新頻度のマジックナンバーを定数化

--- src/Xmobar/Config.hs
+++ src/Xmobar/Config.hs
@@ -29,12 +29,12 @@ mkConfigByDevice = do
   battery <- getBattery
   let runnable =
         [ Run StdinReader
-        , Run $ Cpu ["--ppad", "3"] 100
-        , Run $ CpuFreq ["-t", "Freq: <max>GHz", "--ddigits", "2"] 100
-        , Run $ Memory ["-t", "Mem: <used>M"] 100
-        , Run $ Swap ["-t", "Swap: <used>M"] 100
-        , Run $ DiskIO [("/", "IO: <read>|<write>")] ["--minwidth", "4"] 100
-        , Run $ DynNetwork ["-t", "Net: <rx>KB|<tx>KB", "--minwidth", "2"] 100
+        , Run $ Cpu ["--ppad", "3"] basicRate
+        , Run $ CpuFreq ["-t", "Freq: <max>GHz", "--ddigits", "2"] basicRate
+        , Run $ Memory ["-t", "Mem: <used>M"] basicRate
+        , Run $ Swap ["-t", "Swap: <used>M"] basicRate
+        , Run $ DiskIO [("/", "IO: <read>|<write>")] ["--minwidth", "4"] basicRate
+        , Run $ DynNetwork ["-t", "Net: <rx>KB|<tx>KB", "--minwidth", "2"] basicRate
         ] <>
         (pure . fst) temp <>
         maybe [] (pure . fst) battery <>
@@ -58,14 +58,18 @@ getTemp = do
     else genericTemp
 
 amdTemp :: (Runnable, String)
-amdTemp = (Run $ K10Temp "0000:00:18.3" ["-t", "Temp: <Tctl>°C"] 100, "%k10temp%")
+amdTemp = (Run $ K10Temp "0000:00:18.3" ["-t", "Temp: <Tctl>°C"] basicRate, "%k10temp%")
 
 genericTemp :: (Runnable, String)
-genericTemp = (Run $ MultiCoreTemp ["-t", "Temp: <max>°C"] 100, "%multicoretemp%")
+genericTemp = (Run $ MultiCoreTemp ["-t", "Temp: <max>°C"] basicRate, "%multicoretemp%")
 
 getBattery :: IO (Maybe (Runnable, String))
 getBattery = do
   hostChassis <- getHostChassisNormal
   return $ if hostChassis == HostChassisLaptop
-    then Just (Run $ Battery ["-t", "Bat: <acstatus> <left>%"] 100, "%battery%")
+    then Just (Run $ Battery ["-t", "Bat: <acstatus> <left>%"] basicRate, "%battery%")
     else Nothing
+
+-- | 基本的な更新頻度。
+basicRate :: Rate
+basicRate = 100
