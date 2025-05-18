51e345e09b4e9ccbb3c7d3dedaa28fcba7e3a40d
ncaq <ncaq@ncaq.net>
2025-05-18 11:58:36 +0900

    build(cache): use `https://cache.iog.io`

--- flake.nix
+++ flake.nix
@@ -42,13 +42,13 @@
   nixConfig = {
     extra-substituters = [
       "https://cache.nixos.org/"
+      "https://cache.iog.io"
       "https://nix-community.cachix.org"
-      "https://iohk.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
     ];
     allow-import-from-derivation = "true";
   };
