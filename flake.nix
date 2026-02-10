bbbf5711ce71c42185c506079a67c9b59a6152c0
ncaq <ncaq@ncaq.net>
2026-02-10 20:27:40 +0900

    refactor(nix): haskell.nixからflake-parts構成へ移行
    dotfilesにhaskell.nixを参照させるとキャッシュが効いていても、
    更新のためにそこそこ長いダウンロードの時間がかかります。
    haskell.nixの多数の機能は有用ですが、
    これは個人のウインドウマネージャの構成であり、
    例えばGHCのバージョンの細かいマネジメントは不要だと思ったので、
    シンプルな構成に移行しました。
    
    - flake.nixをhaskell.nix依存からflake-partsとgtk2hs依存に書き換え
    - flake.lockを対応する依存に更新
    - haskell.nixのproject.flake構成を削除し、haskellPackagesを直接定義
    - gtk2hsパッケージをinputsから参照し、無限再帰回避のためpkg-config依存を明示
    - devShellやtreefmt設定をflake-parts方式に移行
    - xmobarやxmonad-launchのビルド方法を調整
    - nixConfigから不要なsubstituterとpublic-keyを削除

--- flake.nix
+++ flake.nix
@@ -1,245 +1,205 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.follows = "nixpkgs-2511";
+    nixpkgs-2511.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    haskellNix = {
-      url = "github:input-output-hk/haskell.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+    gtk2hs = {
+      url = "github:ncaq/gtk2hs/260805611e54e60383c1d98c5c0a29ad9c2efe7f";
+      flake = false;
     };
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      treefmt-nix,
-      haskellNix,
-      ...
-    }:
-    flake-utils.lib.eachSystem [ "aarch64-linux" "x86_64-linux" ] (
-      system:
-      let
-        ghc-version = "ghc9103"; # GHC 9.10.3
-        overlays = [
-          haskellNix.overlay
-          (
-            final: _prev:
-            let
-              # haskell.nixのtoolsで参照されるhaskell-language-server。
-              tool-haskell-language-server =
-                final.haskell-nix.tool ghc-version "haskell-language-server"
-                  "latest";
-            in
-            {
-              # nixpkgsで普通にインストールされるfourmoluはhaskell-language-serverのものと違うので上書きして合わせる。
-              inherit (tool-haskell-language-server.project.hsPkgs.fourmolu.components.exes) fourmolu;
-            }
-          )
-          (final: prev: {
-            project = final.haskell-nix.project' {
-              src = ./.;
-              name = "xmonad-launch";
-              compiler-nix-name = ghc-version;
-              # IFD時にcabalやnix-toolsを実行するシステムを固定。
-              # これによりx86_64-linux上でaarch64-linux用のパッケージも評価可能になる。
-              evalSystem = "x86_64-linux";
-              modules = [
-                # `nix flake check`レベルではcabalの警告をエラーとして扱います。
-                # ライブラリの問題ない範囲の不一致とか考えるとcabalの警告はエラーにしないべきですが、
-                # CIでは通したくないので警告も含めてエラーにします。
-                # CI専用に環境を分離するのも手ですが、
-                # 元々`nix flake check`では最適化を無効にするのが面倒なので時間がかかるため、
-                # あまり反復的に実行しません。
-                # 反復的に実行してテストの結果とかを確認するのには普通は`cabal test`のような言語固有のコマンドを使います。
-                # `cabal test`の方が実行するテストをフィルタリングとかも簡単に出来ますし。
-                # そのことを考えると本番を考えてエラーにしても構わないでしょう。
-                # flake参照された時にnixpkgsをfollowすると問題ない警告をエラーにしてしまうかもしれないのが懸念点ですが、
-                # 少なくとも現在は考慮する必要はないでしょう。
-                # 注意点として、srcやtestはビルドされますが、executableであるappはビルドされません。
-                # 「appはエントリーポイントとしてのみ使う」習慣を守っていれば問題にはならないです。
-                (
-                  { lib, config, ... }:
-                  {
-                    # パッケージたちをハードコーディングすると変更忘れが発生するので`config.package-keys`で取得。
-                    options.packages = lib.genAttrs config.package-keys (
-                      _name:
-                      lib.mkOption {
-                        type = lib.types.submodule (
-                          { config, lib, ... }:
-                          # `cabal.project`に`source-repository-package`などで書かれていたりする、
-                          # 外部パッケージは変更したくないので、
-                          # `isProject`でフィルタリングしています。
-                          lib.mkIf config.package.isProject {
-                            # この書き方で上書きではなく追加として扱われる。
-                            ghcOptions = [ "-Werror" ];
-                          }
-                        );
-                      }
-                    );
-                  }
-                )
-              ];
-              shell = {
-                tools = {
-                  cabal = "latest";
-                  cabal-gild = "latest"; # treefmtで管理されているがvscodeのHaskell拡張向けに使えるようにしておく
-                  haskell-language-server = "latest";
-                  hlint = "latest";
-                  implicit-hie = "latest";
-                };
-                buildInputs = with prev; [
-                  fourmolu
-                  nil
-                  nixfmt-rfc-style
-                  parallel
-                  prev.gtk3
-                  yamllint
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
 
-                  (writeScriptBin "haskell-language-server-wrapper" ''
-                    #!${stdenv.shell}
-                    exec haskell-language-server "$@"
-                  '')
+      imports = [ inputs.treefmt-nix.flakeModule ];
+
+      perSystem =
+        {
+          pkgs,
+          config,
+          lib,
+          ...
+        }:
+        let
+          haskellPackages = pkgs.haskell.packages.ghc9103.override {
+            overrides = hself: hsuper: {
+              # gtk2hs packages from inputs.gtk2hs
+              # callCabal2nixの第3引数でpkg-config依存のHaskellパッケージ名との衝突を解決する。
+              # cabal2nixはpkgconfig-dependsもHaskellパッケージセットから探すため、
+              # glib, cairo等のシステムライブラリ名がHaskellパッケージ名と衝突して無限再帰になる。
+              gtk2hs-buildtools = hself.callCabal2nix "gtk2hs-buildtools" (inputs.gtk2hs + "/tools") { };
+              # cabal2nixはpkgconfig-dependsの名前をそのまま関数引数にするため、
+              # システムライブラリ名がHaskellパッケージ名と衝突する。
+              # 第3引数で明示的にシステムパッケージを渡して衝突を解決する。
+              glib = hself.callCabal2nix "glib" (inputs.gtk2hs + "/glib") {
+                inherit (pkgs) glib;
+              };
+              cairo = hself.callCabal2nix "cairo" (inputs.gtk2hs + "/cairo") {
+                inherit (pkgs) cairo;
+              };
+              gio = hself.callCabal2nix "gio" (inputs.gtk2hs + "/gio") {
+                system-glib = pkgs.glib;
+              };
+              pango = hself.callCabal2nix "pango" (inputs.gtk2hs + "/pango") {
+                inherit (pkgs) pango;
+              };
+              # cabal2nixはgtk3パッケージとして生成する(gtk/ディレクトリだがcabal名がgtk3)
+              gtk3 = hself.callCabal2nix "gtk3" (inputs.gtk2hs + "/gtk") {
+                inherit (pkgs) gtk3;
+              };
+
+              # xmobar with flags
+              xmobar = pkgs.haskell.lib.compose.enableCabalFlag "with_datezone" (
+                pkgs.haskell.lib.compose.enableCabalFlag "with_threaded" hsuper.xmobar
+              );
+
+              xmonad-launch = hself.callCabal2nix "xmonad-launch" inputs.self { };
+            };
+          };
+
+          xmonad-helper-bin = pkgs.runCommand "xmonad-helper-bin" { } ''
+            mkdir -p $out/bin
+            cp ${./bin}/* $out/bin/
+            chmod +x $out/bin/*
+          '';
+
+          xmonad-launch-full = pkgs.symlinkJoin {
+            name = "xmonad-launch-full";
+            paths = with pkgs; [
+              birdtray
+              copyq
+              haskellPackages.xmonad-launch
+              imagemagick
+              networkmanagerapplet
+              oxipng
+              snixembed
+              trayer
+              xmonad-helper-bin
+              xorg.xinput
+            ];
+          };
+        in
+        {
+          packages = {
+            default = xmonad-launch-full;
+          };
+
+          checks = {
+            xmonad-launch = pkgs.haskell.lib.compose.appendConfigureFlags [
+              "--ghc-options=-Werror"
+            ] haskellPackages.xmonad-launch;
+            formatting = config.treefmt.build.check inputs.self;
+          };
+
+          treefmt = {
+            # yamlfmtはprettierと競合します。
+            projectRootFile = "flake.nix";
+            programs = {
+              actionlint.enable = true;
+              deadnix.enable = true;
+              fourmolu.enable = true;
+              hlint.enable = true;
+              nixfmt.enable = true;
+              prettier.enable = true;
+              shellcheck.enable = true;
+              shfmt.enable = true;
+              statix.enable = true;
+            };
+            settings.formatter = {
+              # cabal-gildのモジュール自動発見機能に対応するため、
+              # Haskellソースファイルの変更も検知してcabal-gildを実行します。
+              # treefmt-nixの上流では変更されたファイルだけを修正したいと言われてマージされていませんが、
+              # ローカルで使う分には問題ありません。
+              # [fix(cabal): cabal-fmt and cabal-gild discover module by ncaq · Pull Request #384 · numtide/treefmt-nix](https://github.com/numtide/treefmt-nix/pull/384)
+              cabal-gild = {
+                command = lib.getExe (
+                  pkgs.writeShellApplication {
+                    name = "cabal-gild-wrapper";
+                    runtimeInputs = with pkgs; [
+                      git
+                      haskellPackages.cabal-gild
+                      parallel
+                    ];
+                    text = ''
+                      git ls-files -z "*.cabal" | parallel --null "cabal-gild --io {}"
+                    '';
+                  }
+                );
+                includes = [
+                  "*.cabal"
+                  # Haskellソースファイルの変更を検知するために含める
+                  "*.hs"
+                  "*.lhs"
+                  "*.hsc"
+                  "*.chs"
+                  "*.hsig"
+                  "*.lhsig"
+                ];
+              };
+              editorconfig-checker = {
+                command = lib.getExe (
+                  pkgs.writeShellApplication {
+                    name = "editorconfig-checker-wrapper";
+                    runtimeInputs = [ pkgs.editorconfig-checker ];
+                    text = ''
+                      editorconfig-checker -config .editorconfig-checker.json "$@"
+                    '';
+                  }
+                );
+                includes = [ "*" ];
+                excludes = [
+                  ".direnv/*"
+                  ".git/*"
+                  "dist-newstyle/*"
+                  "result*"
                 ];
               };
             };
-          })
-        ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-          inherit (haskellNix) config;
-        };
-        flake = pkgs.project.flake { };
-        treefmtEval = treefmt-nix.lib.evalModule pkgs (_: {
-          # yamlfmtはprettierと競合します。
-          projectRootFile = "flake.nix";
-          programs = {
-            actionlint.enable = true;
-            deadnix.enable = true;
-            hlint.enable = true;
-            nixfmt.enable = true;
-            prettier.enable = true;
-            shellcheck.enable = true;
-            shfmt.enable = true;
-            statix.enable = true;
+          };
 
-            fourmolu = {
-              enable = true;
-              package = pkgs.fourmolu;
-            };
+          devShells.default = haskellPackages.shellFor {
+            packages = p: [ p.xmonad-launch ];
+            nativeBuildInputs = with pkgs; [
+              cabal-install
+              fourmolu
+              gtk3
+              haskell-language-server
+              haskellPackages.cabal-gild
+              hlint
+              nil
+              nixfmt
+              yamllint
+
+              (pkgs.writeScriptBin "haskell-language-server-wrapper" ''
+                #!${pkgs.stdenv.shell}
+                exec haskell-language-server "$@"
+              '')
+            ];
           };
-          settings.formatter = {
-            # cabal-gildのモジュール自動発見機能に対応するため、
-            # Haskellソースファイルの変更も検知してcabal-gildを実行します。
-            # treefmt-nixの上流では変更されたファイルだけを修正したいと言われてマージされていませんが、
-            # ローカルで使う分には問題ありません。
-            # [fix(cabal): cabal-fmt and cabal-gild discover module by ncaq · Pull Request #384 · numtide/treefmt-nix](https://github.com/numtide/treefmt-nix/pull/384)
-            cabal-gild = {
-              command = pkgs.lib.getExe (
-                pkgs.writeShellApplication {
-                  name = "cabal-gild-wrapper";
-                  runtimeInputs = [
-                    (pkgs.haskell-nix.tool ghc-version "cabal-gild" "1.6.0.2")
-                    pkgs.git
-                    pkgs.parallel
-                  ];
-                  text = ''
-                    git ls-files -z "*.cabal" | parallel --null "cabal-gild --io {}"
-                  '';
-                }
-              );
-              includes = [
-                "*.cabal"
-                # Haskellソースファイルの変更を検知するために含める
-                "*.hs"
-                "*.lhs"
-                "*.hsc"
-                "*.chs"
-                "*.hsig"
-                "*.lhsig"
-              ];
-            };
-            editorconfig-checker = {
-              command = pkgs.lib.getExe (
-                pkgs.writeShellApplication {
-                  name = "editorconfig-checker-wrapper";
-                  runtimeInputs = [ pkgs.editorconfig-checker ];
-                  text = ''
-                    editorconfig-checker -config .editorconfig-checker.json "$@"
-                  '';
-                }
-              );
-              includes = [ "*" ];
-              excludes = [
-                ".direnv/*"
-                ".git/*"
-                "dist-newstyle/*"
-                "result*"
-              ];
-            };
-          };
-        });
-        xmonad-launch = flake.packages."xmonad-launch:exe:xmonad-launch";
-        xmobar-launch = flake.packages."xmonad-launch:exe:xmobar-launch";
-        xmonad-helper-bin = pkgs.runCommand "xmonad-helper-bin" { } ''
-          mkdir -p $out/bin
-          cp ${./bin}/* $out/bin/
-          chmod +x $out/bin/*
-        '';
-        xmonad-launch-full = pkgs.symlinkJoin {
-          name = "xmonad-launch-full";
-          paths = with pkgs; [
-            birdtray
-            copyq
-            imagemagick
-            networkmanagerapplet
-            oxipng
-            snixembed
-            trayer
-            xmobar-launch
-            xmonad-helper-bin
-            xmonad-launch
-            xorg.xinput
-          ];
         };
-      in
-      # haskell.nixのproject.flakeはciJobsとhydraJobsを生成するが、
-      # ciJobsは非標準のoutputであり警告を誘発し、
-      # hydraJobsもGitHub Actionsを使うため不要なので除外。
-      builtins.removeAttrs flake [
-        "ciJobs"
-        "hydraJobs"
-      ]
-      // {
-        checks =
-          flake.packages # テストがないパッケージもビルドしてエラーを検出する。
-          // flake.checks
-          // {
-            formatting = treefmtEval.config.build.check self;
-          };
-        formatter = treefmtEval.config.build.wrapper;
-        packages = flake.packages // {
-          default = xmonad-launch-full;
-          xmobar-launch = flake.packages."xmonad-launch:exe:xmobar-launch";
-          xmonad-launch = flake.packages."xmonad-launch:exe:xmonad-launch";
-        };
-      }
-    );
+    };
 
   nixConfig = {
     extra-substituters = [
       "https://cache.nixos.org/"
-      "https://cache.iog.io"
       "https://nix-community.cachix.org"
       "https://ncaq-xmonad.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "ncaq-xmonad.cachix.org-1:ngWi7GYMyNG5XYhjqRnPphQdViZdZcJ5b+IZ1FD3ebg="
     ];
