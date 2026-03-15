20d4a09c51e44b727dfc0ee3a3b7720851f85c84
ncaq <ncaq@ncaq.net>
2026-03-15 14:24:30 +0900

    ci: 共通キャッシュ設定
    cachixのキャッシュを効率のために原則ncaqに統一することにしました。
    統一して利用している自分のオンプレキャッシュを追加しました。
    flake.nixのものをそのまま受け入れるのはGitHubホストランナーのみです。
    GitHubのキャッシュには触れないはずなので権限を除去しました。

--- flake.nix
+++ flake.nix
@@ -194,13 +194,15 @@
   nixConfig = {
     extra-substituters = [
       "https://cache.nixos.org/"
-      "https://nix-community.cachix.org"
-      "https://ncaq-xmonad.cachix.org"
+      "https://niks3-public.ncaq.net/"
+      "https://ncaq.cachix.org/"
+      "https://nix-community.cachix.org/"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "niks3-public.ncaq.net-1:e/B9GomqDchMBmx3IW/TMQDF8sjUCQzEofKhpehXl04="
+      "ncaq.cachix.org-1:XF346GXI2n77SB5Yzqwhdfo7r0nFcZBaHsiiMOEljiE="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "ncaq-xmonad.cachix.org-1:ngWi7GYMyNG5XYhjqRnPphQdViZdZcJ5b+IZ1FD3ebg="
     ];
     allow-import-from-derivation = true;
   };
